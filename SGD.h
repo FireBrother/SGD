31294ebc09a052537f98b371befe27fb883e2c5f
Xian Wu <wuxian94@pku.edu.cn>
2017-11-30 21:28:41 +0800

    继续加注释；加入shuffle

--- SGD.h
+++ SGD.h
@@ -1,5 +1,8 @@
 //
 // Created by 吴先 on 16/5/27.
+// Last Updated on 17/11/30.
+// SGD基类，实现了基本的数据读入、训练和预测，
+// 需要继承该基类并实现计算概率、loss和梯度的接口
 //
 
 #ifndef SGD_H
@@ -10,11 +13,14 @@
 #include <unordered_map>
 #include <string>
 #include <fstream>
+#include <ctime>
+#include <random>
 #include "limonp/Logging.hpp"
 #include "limonp/StringUtil.hpp"
-#include "time.h"
 #include "utils.h"
 
+// 为了实现方便，使用map作为特征的类型，相同的key表示在相同维度
+// 实际上可以修改成vector，但是在O2的优化下，速度没什么差别
 typedef std::unordered_map<size_t, float> feature_t;
 typedef std::unordered_map<size_t, float> weight_t;
 
@@ -28,10 +34,12 @@ public:
     float predict(const feature_t &X);
 
 protected:
+    // 保存训练数据和测试数据
     std::vector<float> y_s;
     std::vector<feature_t> X_s;
     std::vector<float> y_test_s;
     std::vector<feature_t> X_test_s;
+    // 参数向量，以及偏移量
     weight_t weight;
     float weight0;
 
@@ -40,6 +48,7 @@ protected:
     float THRESH_CONVERGE = 0.001;
     float ALPHA = 0.1;
 
+    // 初始化权重和数据读取
     void _init_weight();
     std::tuple<std::vector<feature_t>, std::vector<float> > _load_data(const std::string &img_filename,
                                                                        const std::string &label_filename);
@@ -67,6 +76,13 @@ void SGD::load_test_data(const std::string &img_filename, const std::string &lab
     XLOG(INFO) << string_format("Load test data finished, %d records loaded: %ds.", X_test_s.size(), et-st);
 }
 
+// 读入数据集，并返回一个特征向量矩阵与标签列表的tuple
+// 这个_load_data是针对mnist数据集编写的，图片大小28*28像素，0～255灰阶，预先做了居中和旋转摆正，可以不从图像处理的角度考虑优化。
+// 具体的数据描述可以参考http://yann.lecun.com/exdb/mnist/
+// 返回值为X_s和y_s，
+// X_s按行存储每张图片，每张图片以行优先的方式存储，每一维代表一个像素的灰度值（0～255），
+// y_s为标签列表，取值范围0～9。
+// 注意！这里只做了数字3的识别，即3为标签1，其他为标签0，作业要求实现对所有数字的识别，即输入一个手写数字图片，返回其最有可能的数字。
 std::tuple<std::vector<feature_t>, std::vector<float> > SGD::_load_data(const std::string &img_filename,
                                                                                       const std::string &label_filename) {
     std::vector<float> y_s;
@@ -82,6 +98,8 @@ std::tuple<std::vector<feature_t>, std::vector<float> > SGD::_load_data(const st
     int magic;
     int num_items;
     int num_rows, num_cols;
+    // 因为是二进制存储的，需要考虑大小端的问题
+    // 如果你的机器在大小端方面出现问题，可以考虑删除reverse。
     img_fin.read((char*)&magic, sizeof(magic));
     std::reverse((char*)&magic, (char*)&magic+sizeof(magic));
     img_fin.read((char*)&num_items, sizeof(num_items));
@@ -106,11 +124,13 @@ std::tuple<std::vector<feature_t>, std::vector<float> > SGD::_load_data(const st
         }
         label_fin.read((char*)&buff, sizeof(buff));
         X_s.push_back(x);
+        // TODO:！！！注意！！！，这里就是只识别数字3的罪魁祸首
         y_s.push_back(float(buff) == 3 ? 1 : 0);
     }
     return std::make_tuple(X_s, y_s);
 }
 
+// 权重的随机初始化，范围-0.5~0.5
 void SGD::_init_weight() {
     srand((unsigned int) time(NULL));
     weight0 = 0;
@@ -124,32 +144,42 @@ void SGD::train() {
     XLOG(INFO) << string_format("Train: MAX_EPOCH=%d, THRESH_CONVERGE=%f, ALPHA=%f",
                                 MAX_EPOCH, THRESH_CONVERGE, ALPHA);
     const time_t st = time(NULL);
+    // 初始化参数向量和训练参数
     _init_weight();
-    float alpha = ALPHA;
-    size_t epoch = 0;
-    float cvg;
-    float tot_cost = _tot_cost();
+    float alpha = ALPHA;    // 学习率
+    size_t epoch = 0;       // 训练轮数
+    float cvg;              // 两次训练的loos之差，用于提前停止
+    float tot_cost = _tot_cost();   // 在整个训练集上的loss
+    // 用于打乱序号，SGD每次训练开始时应该shuffle训练集
+    std::vector<int> idx(X_s.size());
+    for (int i = 0; i < X_s.size(); ++i) {
+        idx.push_back (i);
+    }
     while (true) {
         const time_t ep_st = time(NULL);
         epoch++;
+        std::shuffle(idx.begin(), idx.end(), std::default_random_engine());
         // 每接受一对样本，执行一次梯度下降
         for (size_t i = 0; i < X_s.size(); i++) {
-            weight_t diff = _derived(X_s[i], y_s[i]);
-            weight0 -= alpha * (_p(X_s[i]) - y_s[i]);
+            weight_t diff = _derived(X_s[idx[i]], y_s[idx[i]]); // 计算梯度
+            weight0 -= alpha * (_p(X_s[idx[i]]) - y_s[idx[i]]); // 根据梯度公式，gb=(p-sigmoid(w*x))*1
             for (auto p : diff) {
-                weight[p.first] -= alpha * p.second;
+                weight[p.first] -= alpha * p.second;            // 根据梯度公式，gw=(p-sigmoid(w*x))*x
             }
         }
         // 计算当前模型的loss
         float temp_cost = tot_cost;
         tot_cost = _tot_cost();
+        // 计算loss变化
         cvg = (float) fabs(tot_cost - temp_cost);
         const time_t ep_et = time(NULL);
         XLOG(INFO) << string_format("epoch: %d, cost=%f, alpha=%f: %ds", epoch, tot_cost, alpha, ep_et-ep_st);
+        // 是否已收敛，如果收敛则提前停止
         if (cvg < THRESH_CONVERGE) {
             XLOG(INFO) << "The cost function has converged: cvg=" << cvg;
             break;
         }
+        // 是否到达最大迭代轮数
         if (epoch >= MAX_EPOCH) {
             XLOG(INFO) << "Max epoch reached: epoch=" << epoch;
             break;
